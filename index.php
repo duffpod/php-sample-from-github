2b912f1a3abc0230668015f4fff6c47e6547ab2d
Paul Semionov <duffpod@gmail.com>
2019-09-13 17:30:43 +0300

    Initial commit.

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+
+<head>
+  <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+  <title>My Personal Page</title>
+  <link rel="stylesheet" href="index.css">
+</head>
+
+<body>
+  <div class="content">
+    <?php echo '<h1>Ivan Ivanov</h1>'; ?>
+  </div>
+</body>
+
+</html>
